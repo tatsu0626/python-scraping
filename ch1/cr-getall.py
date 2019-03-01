0e360c525bf893aef2c74cd6bd9a669960bd5829
Yabe Tatsuya <yabe.tatsuya0626@gmail.com>
2019-03-01 23:29:18 +0900

    third commit

--- ch1/cr-getall.py
+++ ch1/cr-getall.py
@@ -45,4 +45,23 @@ def analyze_html(url,root_url):
     savepath=download_file(url)
     if savepath is None:return
     if savepath in proc_files:return
-    
+    proc_files[savepath]=True
+    print("analyze_html=",url)
+    html = open(savepath, "r", encoding="utf-8").read()
+    links = enum_links(html, url)
+    for link_url in links:
+        # リンクがルート以外のパスを指していたら無視 --- (※11)
+        if link_url.find(root_url) != 0:
+            if not re.search(r".css$", link_url): continue
+        # HTMLか？
+        if re.search(r".(html|htm)$", link_url):
+            # 再帰的にHTMLファイルを解析
+            analize_html(link_url, root_url)
+            continue
+        # それ以外のファイル
+        download_file(link_url)
+
+if __name__ == "__main__":
+    # URLを丸ごとダウンロード --- (※13)
+    url = "https://docs.python.jp/3.6/library/"
+    analize_html(url, url)
